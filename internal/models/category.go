2361c96a83a39252496d9467c2ca421e760c5191
a4god- <alexismhgarcia@gmail.com>
2025-03-30 23:13:01 -0400

    changed some category icons

--- internal/models/category.go
+++ internal/models/category.go
@@ -39,11 +39,11 @@ func MigrateCategories(db *gorm.DB) error {
 func (c *Category) Setup(db *gorm.DB) error {
 	categories := []Category{
 		{Title: "Home", Color: "#FFAB91", IconName: "home"},
-		{Title: "Work", Color: "#80D8FF", IconName: "work_outline"},
-		{Title: "Sports", Color: "#81C784", IconName: "sports_soccer"},
-		{Title: "Couple", Color: "#F48FB1", IconName: "favorite"},
-		{Title: "Health", Color: "#EF9A9A", IconName: "favorite_border"},
-		{Title: "Study", Color: "#FFF59D", IconName: "menu_book"},
+		{Title: "Work", Color: "#80D8FF", IconName: "briefcase"},
+		{Title: "Sports", Color: "#81C784", IconName: "soccer"},
+		{Title: "Couple", Color: "#F48FB1", IconName: "heart"},
+		{Title: "Health", Color: "#EF9A9A", IconName: "heart-outline"},
+		{Title: "Study", Color: "#FFF59D", IconName: "book"},
 		{Title: "Shopping", Color: "#CE93D8", IconName: "shopping_cart"},
 		{Title: "Finance", Color: "#A5D6A7", IconName: "attach_money"},
 		{Title: "Travel", Color: "#90CAF9", IconName: "flight"},
